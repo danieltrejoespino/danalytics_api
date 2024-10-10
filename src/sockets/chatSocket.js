599fdea6893d0845fca16b80a7323883f8e8f1c0
Daniel Trejo <daniel.trejo.espino@gmail.com>
2024-10-10 00:40:42 -0600

    create rooms to chat

--- src/sockets/chatSocket.js
+++ src/sockets/chatSocket.js
@@ -1,62 +1,48 @@
-let messages = [];
+const messages = {};  // Mensajes por sala
 
-function addMessage(msg) {
+const addMessage = (data) => {
   
-  if (messages.length >= 30) {
-    messages.shift();
-  }
-  messages.push(msg);
-}
+  if (messages[data.room].length >=30) {
+    messages[data.room].shift();
+  }else{
+    messages[data.room].push(data);  // Guardar el mensaje en la sala correspondiente
+  }  
+
+};
+ 
 
 export const chatSocket = (io) => {
   io.on('connection', (socket) => {
     console.log('Conectado:', socket.id); 
 
-    socket.emit('previousMessages', messages);
-    socket.on('chatMessage', (msg) => {
-      addMessage(msg)
-      
+    socket.on('joinRoom', (room) => {
+      socket.join(room)
+      console.log(`${socket.id} se ha unido a la sala: ${room}`);
 
-      io.emit('chatMessage', msg); 
+      socket.emit('previousMessages', messages[room] || []);
+      socket.to(room).emit('message', `Usuario ${socket.id} se ha unido a la sala`);
+
+    })
+
+    socket.on('chatMessage', (data) => {
+      // console.log(data);
+
+      if (!messages[data.room]) {
+        messages[data.room] = [];  // Si no existe la sala, se crea un array para los mensajes
+      }    //   // Guardar el mensaje en el array de la sala
+      addMessage(data);
+
       console.log(messages);
+
+    //   // Enviar el mensaje solo a los usuarios de la sala
+      io.to(data.room).emit('chatMessage', data); 
+      console.log(`Mensaje en la sala ${data.room}:`, data.MSG);
     });
+
 
     socket.on('disconnect', () => {
       console.log('Usuario desconectado:', socket.id);
     });
   });
 };
-// import jwt from 'jsonwebtoken';
 
-// export const chatSocket = (io) => {
-//   io.use((socket, next) => {
-    
-//     const token = socket.handshake.auth.token;
-//     console.log("Token recibido:", token);
-//     if (!token) {
-//       return next(new Error('Authentication error'));
-//     }
-
-//     jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-//       if (err) {
-//         return next(new Error('Authentication error'));
-//       }
-//       socket.user = decoded;
-//       next();
-//     });
-//   });
-
-//   io.on('connection', (socket) => {
-//     console.log('User connected:', socket.user.username);
-
-//     // Recibir mensajes del cliente
-//     socket.on('chatMessage', (msg) => {
-//       io.emit('chatMessage', { user: socket.user.username, message: msg });
-//     });
-
-//     // Desconectar al usuario
-//     socket.on('disconnect', () => {
-//       console.log('User disconnected:', socket.user.username);
-//     });
-//   });
-// };
